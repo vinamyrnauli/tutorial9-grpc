6d3e4500920f967746ef0f5cf603a61c6ab68ba5
vinamyrnauli <vinasllgn@gmail.com>
2024-04-28 21:54:38 +0700

    Chat Service Implementation

--- src/grpc_server.rs
+++ src/grpc_server.rs
@@ -7,8 +7,8 @@ pub mod services {
     tonic::include_proto!("services");
 }
 
-use services::{payment_service_server::{PaymentService, PaymentServiceServer}, PaymentRequest, PaymentResponse,
-    transaction_service_server::{TransactionService, TransactionServiceServer}, TransactionRequest, TransactionResponse};
+use services::{payment_service_client::PaymentServiceClient, PaymentRequest,transaction_service_client::TransactionServiceClient, TransactionRequest,
+    chat_service_client::ChatServiceClient, ChatMessage};
 
 #[derive(Default)]
 pub struct MyPaymentService {}
@@ -68,6 +68,40 @@ impl TransactionService for MyTransactionService {
     }
 }
 
+#[derive(Default)]
+pub struct MyChatService {}
+
+#[tonic::async_trait]
+impl ChatService for MyChatService {
+    type ChatStream = ReceiverStream<Result<ChatMessage, Status>>;
+
+    async fn chat(
+        &self,
+        request: Request<tonic::Streaming<ChatMessage>>,
+    ) -> Result<Response<Self::ChatStream>, Status> {
+        let mut stream = request.into_inner();
+        let (tx, rx) = mpsc::channel(10);
+
+        tokio::spawn(async move {
+            while let Some(message) = stream.message().await.unwrap_or_else(|_| None) {
+                println!("Received message: {:?}", message);
+                let reply = ChatMessage {
+                    user_id: message.user_id.clone(),
+                    message: format!(
+                        "Terima kasih telah melakukan chat kepada CS virtual,
+                    Pesan anda akan dibalas pada jam kerja. pesan anda: {}",
+                        message.message
+                    ),
+                };
+
+                tx.send(Ok(reply)).await.unwrap_or_else(|_| {});
+            }
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
@@ -77,6 +111,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Server::builder()
         .add_service(PaymentServiceServer::new(payment_service))
         .add_service(TransactionServiceServer::new(transaction_service))
+        .add_service(ChatServiceServer::new(chat_service))
         .serve(addr)
         .await?;
     Ok(())
